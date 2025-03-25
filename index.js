2c7ee349997daa27484083a32bb415e7c2ebd09f
ReaganBaimet0 <reagan.baimet@student.moringaschool.com>
2025-03-25 09:13:18 +0300

    contnuing

--- index.js
+++ index.js
@@ -5,3 +5,14 @@
       .then(response => response.json()) 
       .then(characters => {
         const characterBar = document.getElementById('character-bar');
+        characters.forEach(character => {
+          const span = document.createElement('span');
+          span.textContent = character.name;
+          span.addEventListener('click', () => displayCharacterDetails(character));
+          characterBar.appendChild(span);
+      });
+  });
+}
+function displayCharacterDetails(character) {
+  const detailedInfo = document.getElementById('detailed-info');
+  detailedInfo.innerHTML = `
